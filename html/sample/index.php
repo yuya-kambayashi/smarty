b01e8a8271d3d37b1209f3e4b139b6dfcff3aaae
yuya-kambayashi <kambayashi73@gmail.com>
2022-04-21 16:59:36 +0900

    change include path to relative

--- html/sample/index.php
+++ html/sample/index.php
@@ -1,13 +1,13 @@
 <?php
 
-require("C:/xampp/htdocs/smarty/libs/Smarty.class.php");
+require("../../smarty/libs/Smarty.class.php");
 $smarty = new Smarty();
 $smarty->template_dir = "./templates";
 $smarty->compile_dir = "./templates_c";
 $smarty->cache_dir = "./cache";
 $smarty->config_dir = "./configs";
 
-$smarty->assign( 'name', 'Tarou');
+$smarty->assign( 'name', 'Tarouaaaa');
 $smarty->assign( 'address', 'created by SMARTY and php5 oracle11');
 
 $smarty->assign( 'engine', 'Google');
@@ -19,7 +19,7 @@ $data = array(
     );
 $smarty->assign('personaldata', $data);
 
-$smarty->assign('title', '繰り返し処理のテスト');
+$smarty->assign('title', '繰り返し処理のテストaaa');
 
 
 $smarty->display('index.tpl');
